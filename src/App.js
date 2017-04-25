ee1674bd62579f07b8fc268587c78dcd81bdb805
Yuriy Dybskiy <yuriy@dybskiy.com>
2017-04-24 18:45:29 -0700

    S3 upload wip

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ import './App.css';
 aws.config.update(aws_config);
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {}
+  }
+
   handleImageChange(e) {
     console.log("file changed");
     e.preventDefault();
@@ -17,25 +23,35 @@ class App extends Component {
     reader.readAsDataURL(file);
 
     reader.onloadend = () => {
-      let rekognition = new aws.Rekognition();
+      let bucket = new aws.S3({params: {Bucket: aws_config.bucket}});
+      let params = {Key: file.name, ContentType: file.type, Body: file};
+      bucket.upload(params, function (err, data) {
+        console.log(err, data);
+        let rekognition = new aws.Rekognition();
 
-      var params = {
-        Image: { /* required */
-          Bytes: reader.result,
-        },
-        MaxLabels: 0,
-        MinConfidence: 0.0
-      };
+        var params = {
+          Image: { /* required */
+            S3Object: {
+              Bucket: data.Bucket,
+              Name: data.Key
+            }
+          },
+          MinConfidence: 0.0
+        };
 
-      rekognition.detectLabels(params, function(err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else     console.log(data);           // successful response
+        rekognition.detectLabels(params, (err, data) => {
+          if (err) console.log(err, err.stack); // an error occurred
+          else {
+            this.setState({labels: data})
+          }
+        });
       });
-      console.log(file, reader);
     }
   }
 
   render() {
+    let labels = this.state.labels;
+
     return (
       <div className="App">
         <div className="App-header">
@@ -46,6 +62,11 @@ class App extends Component {
                 type="file"
                 onChange={(e)=>this.handleImageChange(e)} />
             </form>
+        </div>
+        <div>
+          <pre>
+            {labels}
+          </pre>
         </div>
       </div>
     );
